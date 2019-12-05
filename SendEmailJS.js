6f29792e50289f8aefa27316a968cb4d19ef4738
Pratik <pratikgirdhar@hotmail.com>
2019-12-05 18:30:20 -0500

    Update SendEmailJS.js

--- SendEmailJS.js
+++ SendEmailJS.js
@@ -20,7 +20,7 @@ function StartMicrosoftFlowTriggerOperations() {
         
 		//var dataTemplate = "{\r\n    \"emailaddress\":\"{0}\",\r\n    \"emailSubject\": \"{1}\",\r\n    \"emailBody\": \"{2}\"\r\n}"
 		var dataTemplate = "{\r\n    \"PersonID\":\"{0}\",\r\n    \"FirstName\": \"{1}\",\r\n    \"LastName\": \"{2}\",\r\n  \"JobTitle\": \"{3}\",\r\n \"EmailAddress\": \"{4}\"\r\n}"; 
-        var httpPostUrl = "https://prod-45.westus.logic.azure.com:443/workflows/9f1959e2ddac41f1bad3f7bdbdf32982/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=A9Ki-GDmkAmJwUrrxBLr7eb2rtvd-7n73CpA34G3YAk"; 
+        var httpPostUrl = "HOSTURLFROMMICROSOFTFLOW"; // Copy Url from microsoft flow.
         //Call FormatRow function and replace with the values supplied in input controls. 
         dataTemplate = dataTemplate.FormatRow($("#txtPersonID").val(), $("#txtFirstName").val(), $("#txtLastName").val(),$("#txtJobTitle").val(),$("#txtEmailAddress").val());
          var settings = { 
